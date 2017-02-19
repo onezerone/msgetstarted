0cf1b14fe13a6d2384dd69407c945f29968a3f8d
mcarter101 <mcarter@onezerone.com>
2017-02-18 20:40:27 -0500

    Fix typo

--- express.js
+++ express.js
@@ -210,7 +210,7 @@ var initServer = ()=> {
     var app = express();
 	app.set('port', (process.env.PORT || 5000));
     app.use('/', express.static(__dirname));
-	app.user(forceSsl)
+	app.use(forceSsl)
     app.listen(app.get('port'), function () {
         console.log('Node app is running on port', app.get('port'));
     });
